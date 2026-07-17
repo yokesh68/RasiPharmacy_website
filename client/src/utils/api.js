216c62483dff3d4e7edb5079aed4eb7666231250
yokesh68 <yokesh.26j24@kprcas.ac.in>
2026-07-18 01:00:37 +0530

    Configure environment variable for live Render API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://raasi-pharmacy-backend.onrender.com/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://raasi-pharmacy-backend.onrender.com/api';
 
 export const api = {
   /**
